c8d973e84651eed202cf30389624fe814e02fa58
Miłosz Mertka <milosz.mertka@onet.pl>
2022-10-10 14:37:44 +0200

    Implemented insertion sort

--- aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/InsertionSort.java
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/InsertionSort.java
@@ -6,6 +6,24 @@ public class InsertionSort implements Sorting {
 
     @Override
     public void sort(double[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("nums cannot be null");
+        }
+
+        insertionSort(nums);
+    }
+
+    private void insertionSort(double[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            double tmp = nums[i];
+
+            int j = i - 1;
+            while (j >= 0 && nums[j] > tmp) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = tmp;
+        }
     }
 
 }
