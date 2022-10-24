95248965ab5962637c4c19627da2ae838fc03588
Miłosz Mertka <milosz.mertka@onet.pl>
2022-10-24 15:14:16 +0200

    Implemented HashListChaining

--- aisd_lab_3_hash_table/src/main/java/pl/edu/pw/ee/HashListChaining.java
+++ aisd_lab_3_hash_table/src/main/java/pl/edu/pw/ee/HashListChaining.java
@@ -2,13 +2,16 @@ package pl.edu.pw.ee;
 
 import pl.edu.pw.ee.services.HashTable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HashListChaining<T extends Comparable<T>> implements HashTable<T> {
 
     private final Elem nil = null;
-    private final Elem[] hashElems;
+    private final List<Elem> hashElems;
     private int nElem;
 
-    private class Elem<T extends Comparable<T>> {
+    private class Elem {
 
         private T value;
         private Elem next;
@@ -20,51 +23,36 @@ public class HashListChaining<T extends Comparable<T>> implements HashTable<T> {
     }
 
     public HashListChaining(int size) {
-        hashElems = new Elem[size];
-        initializeHash();
+        checkIfSizeIsGreaterThanZero(size);
+        hashElems = new ArrayList<>(size);
+        initializeHash(size);
+    }
+
+    public List<Elem> getHashElems() {
+        return hashElems;
     }
 
     @Override
     public void add(T value) {
-        int hashCode = value.hashCode();
-        int hashId = countHashId(hashCode);
+        checkIfValueIsNotNull(value);
 
-        Elem oldElem = hashElems[hashId];
-        while (oldElem != nil && !oldElem.equals(value)) {
-            oldElem = oldElem.next;
-        }
-        if (oldElem != nil) {
-            oldElem.value = value;
-        } else {
-            hashElems[hashId] = new Elem(value, hashElems[hashId]);
-            nElem++;
-        }
+        addValueToHashElems(value);
     }
 
     @Override
     public T get(T value) {
-        int hashCode = value.hashCode();
-        int hashId = countHashId(hashCode);
+        checkIfValueIsNotNull(value);
 
-        Elem elem = hashElems[hashId];
-
-        while (elem != nil && !elem.value.equals(value)) {
-            elem = elem.next;
-        }
-
-        return elem != nil ? elem.value : nil;
+        Elem elem = findElemByValue(value);
+        return elem != nil ? elem.value : null;
     }
 
     @Override
     public void delete(T value) {
-        if (value == null) {
-            throw new IllegalArgumentException("value cannot be null");
-        }
+        checkIfValueIsNotNull(value);
 
-        int hashCode = value.hashCode();
-        int hashId = countHashId(hashCode);
-
-        Elem currentElem = hashElems[hashId];
+        int hashId = computeHashId(value);
+        Elem currentElem = hashElems.get(hashId);
         Elem previousElem = nil;
 
         while (currentElem != nil && !currentElem.value.equals(value)) {
@@ -72,42 +60,75 @@ public class HashListChaining<T extends Comparable<T>> implements HashTable<T> {
             currentElem = currentElem.next;
         }
 
-        if (currentElem != nil) {
-            if (previousElem == nil) {
-                hashElems[hashId] = currentElem.next;
-            }
-
-            previousElem.next = currentElem.next;
-        }
-
+        deleteElemFromHashElems(currentElem, previousElem, hashId);
     }
 
     public double countLoadFactor() {
-        double size = hashElems.length;
-
-        if (size <= 0) {
-            throw new IllegalStateException("hash table size cannot be lower than or equal 0");
-        }
-
-        return nElem / size;
+        int size = hashElems.size();
+        return (double) nElem / size;
     }
 
-    private void initializeHash() {
-        int n = hashElems.length;
-
-        for (int i = 0; i < n; i++) {
-            hashElems[i] = nil;
+    private void initializeHash(int size) {
+        for (int i = 0; i < size; i++) {
+            hashElems.add(nil);
         }
     }
 
     private int countHashId(int hashCode) {
-        int n = hashElems.length;
+        int n = hashElems.size();
+        return Math.abs(hashCode) % n;
+    }
 
-        if (n <= 0) {
-            throw new IllegalStateException("hash table size cannot be lower than or equal 0");
+    private int computeHashId(T value) {
+        int hashCode = value.hashCode();
+        return countHashId(hashCode);
+    }
+
+    private void addValueToHashElems(T value) {
+        int hashId = computeHashId(value);
+        Elem oldElem = findElemByValue(value);
+
+        if (oldElem != nil) {
+            oldElem.value = value;
+        } else {
+            hashElems.set(hashId, new Elem(value, hashElems.get(hashId)));
+            nElem++;
+        }
+    }
+
+    private Elem findElemByValue(T value) {
+        int hashId = computeHashId(value);
+        Elem elem = hashElems.get(hashId);
+
+        while (elem != nil && !elem.value.equals(value)) {
+            elem = elem.next;
         }
 
-        return Math.abs(hashCode) % n;
+        return elem;
+    }
+
+    private void deleteElemFromHashElems(Elem currentElem, Elem previousElem, int hashId) {
+        if (currentElem != nil) {
+            if (previousElem == nil) {
+                hashElems.set(hashId, currentElem.next);
+            } else {
+                previousElem.next = currentElem.next;
+            }
+
+            nElem--;
+        }
+    }
+
+    private void checkIfValueIsNotNull(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("value cannot be null");
+        }
+    }
+
+    private void checkIfSizeIsGreaterThanZero(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("size cannot be lower than or equal 0");
+        }
     }
 
 }
--- aisd_lab_3_hash_table/src/test/java/pl/edu/pw/ee/HashListChainingTest.java
+++ aisd_lab_3_hash_table/src/test/java/pl/edu/pw/ee/HashListChainingTest.java
@@ -1,5 +1,198 @@
 package pl.edu.pw.ee;
 
+import org.junit.Test;
+import pl.edu.pw.ee.services.HashTable;
+
+import static org.junit.Assert.*;
+
 public class HashListChainingTest {
+
+    private static final int DEFAULT_SIZE = 10;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenSizeIsNegative() {
+        // given
+        int size = -1;
+
+        // when
+        HashTable<String> hashTable = new HashListChaining<>(size);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenSizeIsEqualZero() {
+        // given
+        int size = 0;
+
+        // when
+        HashTable<String> hashTable = new HashListChaining<>(size);
+
+        // then
+        assert false;
+    }
+
+    @Test
+    public void itShouldBeInitializedWithNil() {
+        // given
+        int size = 3;
+
+        // when
+        HashListChaining<String> hashTable = new HashListChaining<>(size);
+
+        // then
+        String[] expected = {null, null, null};
+        assertArrayEquals(expected, hashTable.getHashElems().toArray());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenAddValueIsNull() {
+        // given
+        String value = null;
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+
+        // when
+        hashTable.add(value);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenGetValueIsNull() {
+        // given
+        String value = null;
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+
+        // when
+        hashTable.get(value);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenDeleteValueIsNull() {
+        // given
+        String value = null;
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+
+        // when
+        hashTable.delete(value);
+
+        // then
+        assert false;
+    }
+
+    @Test
+    public void itShouldAddValueToHashTable() {
+        // given
+        String value = "test";
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+
+        // when
+        hashTable.add(value);
+
+        // then
+        String addedValue = hashTable.get(value);
+        assertEquals(addedValue, value);
+    }
+
+    @Test
+    public void itShouldAddValuesWithCollisionToHashTable() {
+        // given
+        String firstValue = "test";
+        String secondValue = "test1";
+        String thirdValue = "test2";
+        int size = 1;
+        HashTable<String> hashTable = new HashListChaining<>(size);
+
+        // when
+        hashTable.add(firstValue);
+        hashTable.add(secondValue);
+        hashTable.add(thirdValue);
+
+        // then
+        String firstAddedValue = hashTable.get(firstValue);
+        String secondAddedValue = hashTable.get(secondValue);
+        String thirdAddedValue = hashTable.get(thirdValue);
+        assertEquals(firstAddedValue, firstValue);
+        assertEquals(secondAddedValue, secondValue);
+        assertEquals(thirdAddedValue, thirdValue);
+    }
+
+    @Test
+    public void itShouldReturnNullWhenValueIsNotFound() {
+        // given
+        String value = "test";
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+
+        // when
+        String result = hashTable.get(value);
+
+        // then
+        assertNull(result);
+    }
+
+    @Test
+    public void itShouldDeleteValueFromHashTable() {
+        // given
+        String value = "test";
+        HashTable<String> hashTable = new HashListChaining<>(DEFAULT_SIZE);
+        hashTable.add(value);
+
+        // when
+        hashTable.delete(value);
+
+        // then
+        String result = hashTable.get(value);
+        assertNull(result);
+    }
+
+    @Test
+    public void itShouldDeleteValueWithCollisionFromHashTable() {
+        // given
+        String firstValue = "test";
+        String secondValue = "test1";
+        String thirdValue = "test2";
+        int size = 1;
+        HashTable<String> hashTable = new HashListChaining<>(size);
+        hashTable.add(firstValue);
+        hashTable.add(secondValue);
+        hashTable.add(thirdValue);
+
+        // when
+        hashTable.delete(secondValue);
+
+        // then
+        String firstAddedValue = hashTable.get(firstValue);
+        String secondAddedValue = hashTable.get(secondValue);
+        String thirdAddedValue = hashTable.get(thirdValue);
+        assertEquals(firstValue, firstAddedValue);
+        assertNull(secondAddedValue);
+        assertEquals(thirdValue, thirdAddedValue);
+    }
+
+    @Test
+    public void itShouldCalculateLoadFactor() {
+        // given
+        String firstValue = "test";
+        String secondValue = "test1";
+        String thirdValue = "test2";
+        int size = 10;
+        HashListChaining<String> hashTable = new HashListChaining<>(size);
+        hashTable.add(firstValue);
+        hashTable.add(secondValue);
+        hashTable.add(thirdValue);
+
+        // when
+        double loadFactor = hashTable.countLoadFactor();
+
+        // then
+        double expected = 0.3;
+        double delta = 0;
+        assertEquals(expected, loadFactor, delta);
+    }
 
 }
