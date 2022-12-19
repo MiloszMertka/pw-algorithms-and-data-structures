3031026971149eab892d6a99e8c02249382e2515
Miłosz Mertka <milosz.mertka@onet.pl>
2022-12-19 19:08:26 +0100

    Implemented LCS algorithm

--- aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
+++ aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
@@ -4,31 +4,96 @@ class LongestCommonSubsequence {
 
     private final String topStr;
     private final String leftStr;
-    
+
     private Cell[][] matrix;
-    
-    public LongestCommonSubsequence(String topStr, String leftStr){
-	validateInputString(topStr);
+
+    public LongestCommonSubsequence(String topStr, String leftStr) {
+        validateInputString(topStr);
         validateInputString(leftStr);
-        
+
         this.topStr = topStr;
         this.leftStr = leftStr;
     }
 
-    public String findLCS(){
-        buildMatrix();
-        
-        StringBuilder LCS = new StringBuilder("");
-        
+    public String findLCS() {
+        buildMatrixIfNull();
+
+        return constructLCSFromMatrix();
+    }
+
+    public void display() {
+        findLCS();
+        printMatrix();
+    }
+
+    private void validateInputString(String input) {
+        if (input == null || input.length() == 0) {
+            throw new IllegalArgumentException("input string cannot be empty");
+        }
+    }
+
+    private void buildMatrixIfNull() {
+        if (matrix == null) {
+            buildMatrix();
+        }
+    }
+
+    private void buildMatrix() {
+        matrix = initializeMatrix();
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                fillCellAtGivenPosition(i, j);
+            }
+        }
+    }
+
+    private Cell[][] initializeMatrix() {
+        matrix = new Cell[leftStr.length() + 1][topStr.length() + 1];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = new Cell();
+            }
+        }
+
+        return matrix;
+    }
+
+    private void fillCellAtGivenPosition(int row, int column) {
+        Cell currentCell = matrix[row][column];
+        Cell topCell = matrix[row - 1][column];
+        Cell leftCell = matrix[row][column - 1];
+        Cell topLeftCell = matrix[row - 1][column - 1];
+
+        if (leftStr.charAt(row - 1) == topStr.charAt(column - 1)) {
+            currentCell.setNum(topLeftCell.getNum() + 1);
+            currentCell.setDir(Direction.TOP_LEFT);
+            return;
+        }
+
+        if (topCell.getNum() >= leftCell.getNum()) {
+            currentCell.setNum(topCell.getNum());
+            currentCell.setDir(Direction.TOP);
+        } else {
+            currentCell.setNum(leftCell.getNum());
+            currentCell.setDir(Direction.LEFT);
+        }
+    }
+
+    private String constructLCSFromMatrix() {
+        StringBuilder LCS = new StringBuilder();
+
         int i = matrix.length - 1;
-        int j = matrix.length - 1;
+        int j = matrix[i].length - 1;
         while (i > 0 && j > 0) {
             Cell cell = matrix[i][j];
-            
+            cell.setChecked(true);
+
             if (cell.getDir() == Direction.TOP_LEFT) {
                 LCS.append(topStr.charAt(j - 1));
             }
-            
+
             switch (cell.getDir()) {
                 case LEFT:
                     j--;
@@ -42,101 +107,183 @@ class LongestCommonSubsequence {
                     break;
             }
         }
-        
-	return LCS.reverse().toString();
+
+        return LCS.reverse().toString();
     }
 
-    public void display(){
-	// TODO
+    private void printMatrix() {
+        String tempTopString = "  " + escapeString(topStr);
+        String tempLeftString = " " + escapeString(leftStr);
 
+        int specialCharactersInTopString = countSpecialCharactersInString(tempTopString);
+        int columnsCount = tempTopString.length() - specialCharactersInTopString;
+
+        printRows(columnsCount, tempTopString, tempLeftString);
     }
-    
-    private void validateInputString(String input) {
-        if (input == null || input.length() == 0) {
-            throw new IllegalArgumentException("input string cannot be empty");
+
+    private String escapeString(String str) {
+        return str.replace("\\", "\\\\")
+                .replace("\t", "\\t")
+                .replace("\b", "\\b")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\f", "\\f")
+                .replace("'", "\\'")
+                .replace("\"", "\\\"");
+    }
+
+    private int countSpecialCharactersInString(String str) {
+        int specialCharacters = 0;
+        for (char character : str.toCharArray()) {
+            if (character == '\\') {
+                specialCharacters++;
+            }
         }
+
+        return specialCharacters;
     }
-    
-    private void buildMatrix() {
-        matrix = initializeMatrix();
-        
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[i].length; j++) {
-                Cell currentCell = matrix[i][j];
-                Cell topCell = matrix[i - 1][j];
-                Cell leftCell = matrix[i][j - 1];
-                Cell topLeftCell = matrix[i - 1][j - 1];
-                printMatrix();
-                
-                if (leftStr.charAt(i - 1) == topStr.charAt(j - 1)) {
-                    currentCell.setNum(topLeftCell.getNum() + 1);
-                    currentCell.setDir(Direction.TOP_LEFT);
+
+    private void printRows(int columnsCount, String topString, String leftString) {
+        printTopStringRow(columnsCount, topString);
+
+        for (int i = 0; i < matrix.length; i++) {
+            printBorderRow(columnsCount);
+            printTopCellPartRow(matrix[i]);
+
+            for (int j = -1; j < matrix[i].length; j++) {
+                if (j == -1) {
+                    char character = leftString.charAt(i);
+
+                    if (character == '\\') {
+                        i++;
+                        System.out.print("|  " + character + leftString.charAt(i) + "   ");
+                    } else {
+                        System.out.print("|   " + character + "   ");
+                    }
+
                     continue;
                 }
-                
-                if (topCell.getNum() >= leftCell.getNum()) {
-                    currentCell.setNum(topCell.getNum());
-                    currentCell.setDir(Direction.TOP);
+
+                Cell cell = matrix[i][j];
+
+                if (cell.isChecked() && cell.getDir() == Direction.LEFT) {
+                    System.out.print("| < " + cell.getNum() + "   ");
                 } else {
-                    currentCell.setNum(leftCell.getNum());
-                    currentCell.setDir(Direction.LEFT);
+                    System.out.print("|   " + cell.getNum() + "   ");
                 }
             }
+
+            System.out.println("|");
+
+            printBottomCellPartRow(columnsCount);
         }
+
+        printBorderRow(columnsCount);
     }
-    
-    private Cell[][] initializeMatrix() {
-        matrix = new Cell[leftStr.length() + 1][topStr.length() + 1];
-        
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = new Cell();
+
+    private void printTopStringRow(int columnsCount, String topString) {
+        printBorderRow(columnsCount);
+        printTopCellPartRow(columnsCount);
+        printMiddleCellPartRow(topString);
+        printBottomCellPartRow(columnsCount);
+    }
+
+    private void printBorderRow(int columnsCount) {
+        for (int i = 0; i < columnsCount; i++) {
+            System.out.print("+-------");
+        }
+
+        System.out.println("+");
+    }
+
+    private void printTopCellPartRow(int columnsCount) {
+        for (int i = 0; i < columnsCount; i++) {
+            System.out.print("|       ");
+        }
+
+        System.out.println("|");
+    }
+
+    private void printTopCellPartRow(Cell[] row) {
+        for (int i = -1; i < row.length; i++) {
+            if (i >= 0) {
+                Cell cell = row[i];
+
+                if (cell.isChecked()) {
+                    if (cell.getDir() == Direction.TOP) {
+                        System.out.print("|   ^   ");
+                        continue;
+                    } else if (cell.getDir() == Direction.TOP_LEFT) {
+                        System.out.print("| \\     ");
+                        continue;
+                    }
+                }
+            }
+
+            System.out.print("|       ");
+        }
+
+        System.out.println("|");
+    }
+
+    private void printMiddleCellPartRow(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+
+            if (character == '\\') {
+                i++;
+                System.out.print("|  " + character + str.charAt(i) + "   ");
+            } else {
+                System.out.print("|   " + character + "   ");
             }
         }
-        
-        return matrix;
+
+        System.out.println("|");
     }
-    
-    private void printMatrix() {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                Cell cell = matrix[i][j];
-                
-                System.out.print(cell.getNum() + " ");
-            }
-            
-            System.out.println();
+
+    private void printBottomCellPartRow(int columnsCount) {
+        for (int i = 0; i < columnsCount; i++) {
+            System.out.print("|       ");
         }
-        
-        System.out.println();
+
+        System.out.println("|");
     }
-    
-    private static class Cell {
-        private int num = 0;
-        private Direction dir = null;
-        
-        public int getNum() {
-            return this.num;
-        }
-        
-        public Direction getDir() {
-            return this.dir;
-        }
-        
-        public void setNum(int num) {
-            this.num = num;
-        }
-        
-        public void setDir(Direction dir) {
-            this.dir = dir;
-        }
-         
-    }
-    
-    private static enum Direction {
+
+    private enum Direction {
         TOP,
         LEFT,
         TOP_LEFT
+    }
+
+    private static class Cell {
+
+        private int num = 0;
+        private Direction dir = null;
+        private boolean checked = false;
+
+        public int getNum() {
+            return this.num;
+        }
+
+        public void setNum(int num) {
+            this.num = num;
+        }
+
+        public Direction getDir() {
+            return this.dir;
+        }
+
+        public void setDir(Direction dir) {
+            this.dir = dir;
+        }
+
+        public boolean isChecked() {
+            return checked;
+        }
+
+        public void setChecked(boolean checked) {
+            this.checked = checked;
+        }
     }
 
 }
--- aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
+++ aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
@@ -1,23 +1,151 @@
 package pl.edu.pw.ee;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class LongestCommonSubsequenceTest 
-{
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestCommonSubsequenceTest {
+
+    private static final String DISPLAY_TEST_FILE = "test_files/display_test.txt";
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOutputStream = System.out;
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOutputStream);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenStringIsNull() {
+        // given
+        String topStr = null;
+        String leftStr = null;
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        LCS.findLCS();
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenStringIsEmpty() {
+        // given
+        String topStr = "test";
+        String leftStr = "";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        LCS.findLCS();
+
+        // then
+        assert false;
+    }
+
     @Test
     public void itShouldCorrectlyFindLCS() {
         // given
         String topStr = "KANAPKI";
         String leftStr = "NAPISY";
         LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
-        
+
         // when
         String result = LCS.findLCS();
-        
+
         // then
         String expected = "NAPI";
         assertEquals(expected, result);
     }
+
+    @Test
+    public void itShouldCorrectlyFindLCSWhenSpecialChars() {
+        // given
+        String topStr = "\tczęsto_z_\rodkrywaniem\n";
+        String leftStr = "rzeczy_nie_trzeba\n_się_spieszyć\n";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        String result = LCS.findLCS();
+
+        // then
+        String expected = "cz__raie\n";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void itShouldCorrectlyFindLCSWhenIdenticalStrings() {
+        // given
+        String topStr = "to samo";
+        String leftStr = "to samo";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        String result = LCS.findLCS();
+
+        // then
+        String expected = "to samo";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void itShouldCorrectlyFindLCSWhenSubstring() {
+        // given
+        String topStr = "program";
+        String leftStr = "zaprogramowane";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        String result = LCS.findLCS();
+
+        // then
+        String expected = "program";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void itShouldReturnEmptyStringWhenNoLCS() {
+        // given
+        String topStr = "abcdef";
+        String leftStr = "ghijkl";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        String result = LCS.findLCS();
+
+        // then
+        String expected = "";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void itShouldPrintMatrixCorrectly() throws IOException {
+        // given
+        String topStr = "często_z_odkrywaniem\t";
+        String leftStr = "rzeczy_nie_trzeba\n_się_spieszyć";
+        LongestCommonSubsequence LCS = new LongestCommonSubsequence(topStr, leftStr);
+
+        // when
+        LCS.display();
+
+        // then
+        String result = outputStream.toString();
+        String expected = new String(Files.readAllBytes(Paths.get(DISPLAY_TEST_FILE)));
+        assertEquals(expected, result);
+    }
+
 }
