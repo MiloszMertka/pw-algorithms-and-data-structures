12f954ca56a01db4fa7f60eeb733423825035cac
Miłosz Mertka <milosz.mertka@onet.pl>
2023-01-04 20:37:08 +0100

    Implemented Prim Algorithm

--- aisd_lab_10_min_spannig_tree/src/main/java/pl/edu/pw/ee/PrimAlgorithm.java
+++ aisd_lab_10_min_spannig_tree/src/main/java/pl/edu/pw/ee/PrimAlgorithm.java
@@ -1,80 +1,214 @@
 package pl.edu.pw.ee;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.List;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 import pl.edu.pw.ee.services.MinSpanningTree;
 
 public class PrimAlgorithm implements MinSpanningTree {
 
-    private Double matrix[][];
-    private Queue<Edge> edgesQueue = new PriorityQueue<>();
-    private List<Integer> visitedNodes = new ArrayList<>();
+    private static final String FILE_FORMAT_ERROR = "File is not in correct format!";
+    private static final String PATH_ERROR = "Path to file cannot be null or blank!";
+    private static final String FILE_IO_ERROR = "Cannot read file ";
+    private static final String GRAPH_CONNECTION_ERROR = "Graph is not connected!";
+
+    private final List<Node> graph = new ArrayList<>();
+    private final Queue<Edge> edgesQueue = new PriorityQueue<>();
+    private final Set<Node> visitedNodes = new HashSet<>();
 
     @Override
     public String findMST(String pathToFile) {
-        readGraph();
+        validatePathToFile(pathToFile);
+        clearAlgorithmState();
+        readFile(pathToFile);
 
-        String result = "";
-        addEdgesToQueue(0);
+        return runPrimAlgorithm();
+    }
+
+    private void validatePathToFile(String pathToFile) {
+        if (pathToFile == null || pathToFile.isBlank()) {
+            throw new IllegalArgumentException(PATH_ERROR);
+        }
+    }
+
+    private void clearAlgorithmState() {
+        graph.clear();
+        edgesQueue.clear();
+        visitedNodes.clear();
+    }
+
+    private String runPrimAlgorithm() {
+        StringBuilder result = new StringBuilder();
+
+        addEdgesToQueue(graph.get(0));
+        visitedNodes.add(graph.get(0));
+
         while (!edgesQueue.isEmpty()) {
             Edge edge  = edgesQueue.poll();
-            
-            if (!checkIfNodesAreVisited(edge)) {               
-                if (!result.isEmpty()) {
-                    result += "|";
-                }
-                
-                result += edge.from + "_" + edge.wage + "_" + edge.to;
-                
-                addEdgesToQueue(edge.to);
+
+            if (!checkIfNodesAreVisited(edge)) {
+                appendEdgeToResult(result, edge);
             }
         }
 
-        return result;
+        validateGraphIsConnected();
+
+        return result.toString();
     }
 
-    private void initializeMatrix(int size) {
-        matrix = new Double[size][size];
+    private void appendEdgeToResult(StringBuilder result, Edge edge) {
+        if (result.length() > 0) {
+            result.append("|");
+        }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = null;
-            }
+        Node from = findNodeByName(edge.from);
+        Node to = findNodeByName(edge.to);
+        result.append(from.name).append("_").append(edge.wage).append("_").append(to.name);
+
+        addEdgesToQueue(to);
+        visitedNodes.add(to);
+    }
+
+    private Node findNodeByName(String name) {
+        return graph.stream().filter(node -> node.name.equals(name)).findFirst().get();
+    }
+
+    private void validateGraphIsConnected() {
+        if (visitedNodes.size() != graph.size()) {
+            throw new IllegalStateException(GRAPH_CONNECTION_ERROR);
         }
     }
 
-    private void readGraph() {
-        initializeMatrix(5);
+    private void readFile(String pathToFile) {
+        File file = new File(pathToFile);
 
-        matrix[0][1] = 3.0;
-        matrix[0][2] = 5.0;
-        matrix[0][3] = 7.0;
-        matrix[1][2] = 1.0;
-        matrix[3][4] = 7.0;
+        try (Scanner scanner = new Scanner(file)) {
+            readGraph(scanner);
+        } catch (IOException ioException) {
+            throw new IllegalStateException(FILE_IO_ERROR + ioException);
+        }
     }
 
-    private void addEdgesToQueue(int nodeIndex) {
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[nodeIndex][i] != null) {
-                Edge edge = new Edge(nodeIndex, i, matrix[nodeIndex][i]);
-                edgesQueue.add(edge);
-            }
+    private void readGraph(Scanner scanner) {
+        while (scanner.hasNextLine()) {
+            Edge edge = readEdge(scanner);
+            addEdgeToGraph(edge);
         }
+    }
+
+    private Edge readEdge(Scanner scanner) {
+        validateNextTokenIsString(scanner);
+        String from = scanner.next();
+
+        validateNextTokenIsString(scanner);
+        String to = scanner.next();
+
+        validateNextTokenIsInt(scanner);
+        int wage = scanner.nextInt();
+        validateWageIsPositive(wage);
+
+        Edge edge = new Edge(from, to, wage);
+        validateEdgeHasDifferentNodes(edge);
+
+        return edge;
+    }
+
+    private void validateNextTokenIsString(Scanner scanner) {
+        if (!scanner.hasNext("[a-zA-Z]+")) {
+            throw new IllegalStateException(FILE_FORMAT_ERROR);
+        }
+    }
+
+    private void validateNextTokenIsInt(Scanner scanner) {
+        if (!scanner.hasNextInt()) {
+            throw new IllegalStateException(FILE_FORMAT_ERROR);
+        }
+    }
+
+    private void validateWageIsPositive(int wage) {
+        if (wage <= 0) {
+            throw new IllegalStateException(FILE_FORMAT_ERROR);
+        }
+    }
+
+    private void validateEdgeHasDifferentNodes(Edge edge) {
+        if (edge.from.equals(edge.to)) {
+            throw new IllegalStateException(FILE_FORMAT_ERROR);
+        }
+    }
+
+    private void addEdgeToGraph(Edge edge) {
+        Node fromNode = new Node(edge.from);
+        fromNode.addEdge(edge);
+
+        Node toNode = new Node(edge.to);
+        toNode.addEdge(edge);
+
+        addOrUpdateNodeInGraph(fromNode, edge);
+        addOrUpdateNodeInGraph(toNode, edge);
+    }
+
+    private void addOrUpdateNodeInGraph(Node node, Edge edge) {
+        if (graph.contains(node)) {
+            Node nodeInGraph = graph.get(graph.indexOf(node));
+            nodeInGraph.addEdge(edge);
+        } else {
+            graph.add(node);
+        }
+    }
+
+    private void addEdgesToQueue(Node node) {
+        edgesQueue.addAll(node.edges);
     }
     
     private boolean checkIfNodesAreVisited(Edge edge) {
-        return visitedNodes.contains(edge.from) && visitedNodes.contains(edge.to);
+        boolean fromPresent = checkIfNodeIsVisitedByName(edge.from);
+        boolean toPresent = checkIfNodeIsVisitedByName(edge.to);
+
+        return fromPresent && toPresent;
+    }
+
+    private boolean checkIfNodeIsVisitedByName(String name) {
+        return visitedNodes.stream().anyMatch(node -> node.name.equals(name));
+    }
+
+    private static class Node {
+
+        private final String name;
+        private final Set<Edge> edges = new HashSet<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public void addEdge(Edge edge) {
+            edges.add(edge);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) {
+                return true;
+            }
+
+            if (other == null || getClass() != other.getClass()) {
+                return false;
+            }
+
+            Node node = (Node) other;
+            return name.equals(node.name);
+        }
+
     }
 
     private static class Edge implements Comparable<Edge> {
 
-        private int from;
-        private int to;
-        private double wage;
+        private final String from;
+        private final String to;
+        private final int wage;
 
-        public Edge(int from, int to, double wage) {
+        public Edge(String from, String to, int wage) {
             this.from = from;
             this.to = to;
             this.wage = wage;
@@ -82,15 +216,7 @@ public class PrimAlgorithm implements MinSpanningTree {
 
         @Override
         public int compareTo(Edge other) {
-            if (this.wage > other.wage) {
-                return 1;
-            }
-
-            if (this.wage == other.wage) {
-                return 0;
-            }
-
-            return -1;
+            return Integer.compare(this.wage, other.wage);
         }
 
     }
--- aisd_lab_10_min_spannig_tree/src/test/java/pl/edu/pw/ee/PrimAlgorithmTest.java
+++ aisd_lab_10_min_spannig_tree/src/test/java/pl/edu/pw/ee/PrimAlgorithmTest.java
@@ -3,13 +3,122 @@ package pl.edu.pw.ee;
 import org.junit.Test;
 import pl.edu.pw.ee.services.MinSpanningTree;
 
+import static org.junit.Assert.assertEquals;
+
 public class PrimAlgorithmTest {
 
-    private MinSpanningTree prim = new PrimAlgorithm();
+    private static final String TEST_FILES_PATH = "./test_files/";
+
+    private final MinSpanningTree prim = new PrimAlgorithm();
     
     @Test
     public void itShouldCorrectlyFindMST() {
-        System.out.println(prim.findMST(null));
+        // given
+        String pathToFile = TEST_FILES_PATH + "correct_small_data.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        String expected = "A_3_B|B_1_C|C_1_D|D_7_E";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void itShouldCorrectlyFindMSTWhenOneEdge() {
+        // given
+        String pathToFile = TEST_FILES_PATH + "one_edge.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        String expected = "A_3_B";
+        assertEquals(expected, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenNullPath() {
+        // given
+        String pathToFile = null;
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void itShouldThrowExceptionWhenBlankPath() {
+        // given
+        String pathToFile = "   ";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void itShouldThrowExceptionWhenUnconnectedGraph() {
+        // given
+        String pathToFile = "unconnected.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void itShouldThrowExceptionWhenNegativeWage() {
+        // given
+        String pathToFile = "negative_wage.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void itShouldThrowExceptionWhenWrongFileFormat() {
+        // given
+        String pathToFile = "wrong_format.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void itShouldThrowExceptionWhenFileNotFound() {
+        // given
+        String pathToFile = "not_found.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void itShouldThrowExceptionWhenLoopInGraph() {
+        // given
+        String pathToFile = "loop.txt";
+
+        // when
+        String result = prim.findMST(pathToFile);
+
+        // then
+        assert false;
     }
     
 }
